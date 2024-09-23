1ba34fd98d9cfa3e69fb137dcd1493fdfa123c07
Wijayarathna S.K.N.H <81549684+NipunHWE98@users.noreply.github.com>
2024-09-23 16:23:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 from sklearn.ensemble import GradientBoostingClassifier
-from xgboost import XGBClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report, roc_auc_score
 import pandas as pd
 import pickle
